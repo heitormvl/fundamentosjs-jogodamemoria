3ca60d18e985617754c5eb79b5afcdd579ad1229
heitormvl <heitormvl12@gmail.com>
2022-05-04 01:04:47 -0300

    Front-end update

--- desafio-final/aula02-imagens-dinamicas/src/tela.js
+++ desafio-final/aula02-imagens-dinamicas/src/tela.js
@@ -2,8 +2,8 @@ const ID_CONTEUDO = "conteudo"
 class Tela {
     static obterCodigoHtml(item) {
         return `
-        <div class="col-md-3">
-            <div class="card" style="width: 50%;">
+        <div class="col-md-3 col-sm-3 col-xs-3">
+            <div class="card" style="width: 50%;margin-left:auto;margin-right:auto">
             <img src="${item.img}" name="${item.nome}" class="card-img-top" alt="...">
             </div>
             <br>
